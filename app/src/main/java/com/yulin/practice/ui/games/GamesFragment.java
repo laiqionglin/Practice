2ed2fffe1f49f6cb454c0c2343690ce591d86cae
laiqionglin <410503480@qq.com>
2025-02-03 22:39:55 +0800

    2

--- app/src/main/java/com/yulin/practice/ui/games/GamesFragment.java
+++ app/src/main/java/com/yulin/practice/ui/games/GamesFragment.java
@@ -1,20 +1,30 @@
 package com.yulin.practice.ui.games;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.yulin.practice.R;
 import com.yulin.practice.databinding.FragmentGamesBinding;
+import com.yulin.practice.ui.games.game.MineSweepingActivity;
 
 public class GamesFragment extends Fragment {
 
     private FragmentGamesBinding binding;
+
+    private GridView mGridView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,11 +32,44 @@ public class GamesFragment extends Fragment {
                 new ViewModelProvider(this).get(GamesViewModel.class);
 
         binding = FragmentGamesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        mGridView = binding.gridGames;
+        mGridView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 1;
+            }
 
-        final TextView textView = binding.textGames;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (null == convertView) {
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item_games, parent, false);
+                }
+                ImageView imageView = convertView.findViewById(R.id.grid_item_image);
+                TextView textView = convertView.findViewById(R.id.grid_item_text);
+                textView.setText(getResources().getText(R.string.mine_sweeping));
+                return convertView;
+            }
+        });
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (0 == position) {
+                    Intent intent = new Intent(getActivity(), MineSweepingActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        return binding.getRoot();
     }
 
     @Override
--- app/src/main/java/com/yulin/practice/ui/games/GamesViewModel.java
+++ app/src/main/java/com/yulin/practice/ui/games/GamesViewModel.java
@@ -10,7 +10,7 @@ public class GamesViewModel extends ViewModel {
 
     public GamesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is games fragment");
     }
 
     public LiveData<String> getText() {
